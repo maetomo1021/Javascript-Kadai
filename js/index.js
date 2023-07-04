a5afda034b60546a5c0083dfa28068b9e9acd70d
maetomo1021 <maetomoda@gmail.com>
2023-07-05 01:54:17 +0900

    javascriptでJqueryを使用せずにSort機能を実施しました

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
-
 window.addEventListener("DOMContentLoaded", function () {
   var tokyo = document.getElementsByClassName("Tokyo");
   var osaka = document.getElementsByClassName("Oosaka");
   var tiba = document.getElementsByClassName("Tiba");
   var sizuoka = document.getElementsByClassName("Sizuoka");
   // var test = document.getElementsByClassName("Sizuoka");
-
 
   // console.log(test[0])
   // console.log(test[1])
@@ -23,8 +21,6 @@ window.addEventListener("DOMContentLoaded", function () {
 
   removebutton.addEventListener("click", function () {
     // test.style.display = "none";
-
-
     returnValue = clickData[0].selectedIndex;
 
     // console.log(returnValue)
@@ -35,35 +31,80 @@ window.addEventListener("DOMContentLoaded", function () {
       console.log(sizuoka.length)
 
       console.log("東京のお店を非表示にします")
-      for (let i = 0; i <= 4; i++) {
-        if (tokyo[i].style.display != "none") {
-          tokyo[i].style.display = "none";
-          // test[1].style.display = "none";
-          // test[2].style.display = "none";
-          // test[3].style.display = "none";
-          // test[4].style.display = "none";
-        } else if (tokyo[i].style.display == "none") {
-          console.log("東京を再表示します")
-          // tokyo[i].style.display = "visible"
-          // osaka[i].style.display = "visible"
-          // tiba[i].style.display = "visible"
-
-        }
-      }
-
-    } else if (returnValue == 1) {
-      console.log("大阪のお店を非表示にします")
+      tokyo[0].style.display = "";
+      tokyo[1].style.display = "";
+      tokyo[2].style.display = "";
+      tokyo[3].style.display = "";
+      tokyo[4].style.display = "";
       osaka[0].style.display = "none";
       osaka[1].style.display = "none";
-
-    } else if (returnValue == 2) {
-      console.log("千葉のお店を非表示にします")
+      sizuoka[0].style.display = "none";
+      sizuoka[1].style.display = "none";
       tiba[0].style.display = "none";
 
-    } else if (returnValue == 3) {
-      console.log("静岡のお店を非表示にします")
-      sizuoka[0].style.display = "none";
+      
+      // for (let i = 0; i <= 4; i++) {
+      //   if (tokyo[i].style.display != "") {
+      //     tokyo[i].style.display = "none";
 
+
+
+      //     // test[1].style.display = "none";
+      //     // test[2].style.display = "none";
+      //     // test[3].style.display = "none";
+      //     // test[4].style.display = "none";
+      //   } else if (tokyo[i].style.display == "none") {
+      //     console.log("東京のお店だけを表示します")
+      //     tokyo[i].style.display == "none"
+
+
+      //   }
+      // }
+
+    } else if (returnValue == 1) {
+      console.log("大阪のお店だけを表示します")
+      // osaka[0].style.display = "none";
+      // osaka[1].style.display = "none";
+      osaka[0].style.display = "";
+      osaka[1].style.display = "";
+      sizuoka[0].style.display = "none";
+      sizuoka[1].style.display = "none";
+      tiba[0].style.display = "none";
+      tokyo[0].style.display = "none";
+      tokyo[1].style.display = "none";
+      tokyo[2].style.display = "none";
+      tokyo[3].style.display = "none";
+      tokyo[4].style.display = "none";
+
+
+    } else if (returnValue == 2) {
+      console.log("千葉のお店だけを表示します")
+      tiba[0].style.display= "";
+      // tiba[0].style.display = "none";
+      tokyo[0].style.display = "none";
+      tokyo[1].style.display = "none";
+      tokyo[2].style.display = "none";
+      tokyo[3].style.display = "none";
+      tokyo[4].style.display = "none";
+      osaka[0].style.display = "none";
+      osaka[1].style.display = "none";
+      sizuoka[0].style.display = "none";
+      sizuoka[1].style.display = "none";
+
+
+
+    } else if (returnValue == 3) {
+      console.log("静岡のお店だけを表示します")
+      sizuoka[0].style.display = "";
+      sizuoka[1].style.display = "";
+      tokyo[0].style.display = "none";
+      tokyo[1].style.display = "none";
+      tokyo[2].style.display = "none";
+      tokyo[3].style.display = "none";
+      tokyo[4].style.display = "none";
+      osaka[0].style.display = "none";
+      osaka[1].style.display = "none";
+      tiba[0].style.display= "none";
     }
     else {
       console.log("")
@@ -72,4 +113,25 @@ window.addEventListener("DOMContentLoaded", function () {
 })
 
 
+//大阪非表示
+// osaka[0].style.display = "none";
+// osaka[1].style.display = "none";
 
+//静岡非表示
+// sizuoka[0].style.display = "none";
+// sizuoka[1].style.display = "none";
+
+//千葉非表示
+// tiba[0].style.display = "none";
+
+//東京非表示
+// tokyo[0].style.display = "none";
+// tokyo[1].style.display = "none";
+// tokyo[2].style.display = "none";
+// tokyo[3].style.display = "none";
+
+
+
+
+
+// tokyo[i].style.display = "none";
